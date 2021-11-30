876a03f4afe39658d6c8a08af765e3598b5757ec
AstraTCC <95264377+AstraTCC@users.noreply.github.com>
2021-11-30 16:18:15 -0300

    Add files via upload

---
+++ Astra/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet"  href="style.css">
+        <title>ASTRA</title>
+        <link href='https://fonts.googleapis.com/css?family=Lato:400,100,300' rel='stylesheet' type='text/css'>
+
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,400,700" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Oswald:200,400,700" rel="stylesheet">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+<script src="https://use.fontawesome.com/20ab91acc4.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1"><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'><link rel="stylesheet" href="./style.css">
+
+    </head>
+    <body>
+        <div id="menu-principal">
+            
+            <div class="menu">
+                <ul>
+                    <a href="#contact"><li>FALE CONOSCO</li></a>
+                    <a href="#projeto"><li>PROJETOS</li></a>
+                    <a href="#quemsomos"><li>QUEM SOMOS</li></a>
+                    <a href="#menu-principal"><li>HOME</li></a>
+                </ul>
+                <div class="logo"></div>
+            </div>
+            <div class="imagem">
+                <img src="img/logo2.png">
+            </div>
+        </div>
+
+<div class="pag">
+
+  <div id="quemsomos" class="quemsomos">
+    <div class="anunciado"><h1>EQUIPE</h1></div>
+    <div class="quemsomos2">
+                <div class="foto">
+                    <img src="img/arl.jpeg">
+                    <div class="nome"> <h1>Arlivan Emanuel </h1></div> 
+                </div>
+                <div class="foto">
+                    <img src="img/dom.jpeg">
+                    <div class="nome"> <h1>Domini Gabriel</h1></div> 
+                </div>
+                <div class="foto">
+                    <img src="img/ju.jpeg">
+                    <div class="nome"> <h1>Julia Batista</h1></div> 
+                </div>
+                <div class="foto">
+                    <img src="img/rod.png">
+                    <div class="nome"> <h1>Rodrigo Gonçalves</h1></div> 
+                </div>
+                <div class="foto">
+                    <img src="img/tai.png">
+                    <div class="nome"> <h1>Thainá Ribeiro</h1></div>                    
+                </div>
+
+    </div>
+
+
+  </div>
+
+    <br> <br>
+
+    <div id="projeto"class="projeto">
+      <div class="centralizar">
+      <div class="projeto-img">
+        <img src="img/projeto.png" height="390" width="457">
+      </div>
+      <div class="projeto-info">
+        <div class="projeto-text">
+          <h1>Projetos</h1>
+          <h2>newjobs</h2>
+          <p>Astra conta com seu principal projeto: New Jobs,
+              um site que visa trazer mais vagas de emprego
+              para as comunidades. </p>
+        </div>
+        <div class="projeto-btn">
+        
+          <button type="button">saiba mais</button>
+        </div>
+      </div>
+      </div>
+    </div>
+
+
+        <!-- 
+
+
+
+   fale conosco --> 
+             <!-- <div class="div5">
+              <img src="img/faleco.png"> 
+            </div> -->
+
+<section id="contact" class="contact">
+  <h1 class="section-header">Fale conosco</h1> 
+  <div class="contact-wrapper"> 
+  
+     <!-- formulario -->    
+     <form id="contact-form" class="form-horizontal" role="form">
+       
+         <div class="form-group">
+            <div class="col-sm-12">
+              <input type="text" class="form-control" id="name" placeholder="nome" name="name" value="" required/>
+            </div>
+         </div>
+
+         <div class="form-group">
+           <div class="col-sm-12">
+             <input type="email" class="form-control" id="email" placeholder="email" name="email" value="" required/>
+            </div>
+         </div>
+
+    <textarea class="form-control" rows="10" placeholder="mensagem" name="message" required></textarea>
+      
+         <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">
+           <div class="alt-send-button">
+             <i class="fa fa-paper-plane"></i><span class="send-text">enviar</span>
+          </div>
+      
+      </button>      
+    </form> 
+    
+  <!-- tell,email --> 
+     
+       <div class="direct-contact-container">
+
+          <li class="list-item"><i class="fa fa-phone fa-2x"><span class="contact-text phone"><a href="tel:1-212-555-5555" title="Give me a call">
+            (11)2552-4773</a></span></i></li>
+          
+          <li class="list-item"><i class="fa fa-phone fa-2x"><span class="contact-text phone2"><a href="tel:1-212-555-5555" title="Give me a call">(11)2551-9484
+            </a></span></i></li>
+
+          <li class="list-item"><i class="fa fa-envelope fa-2x"><span class="contact-text gmail"><a href="mailto:#" title="Send me an email">@etecdeguaianazes.com.br</a></span></i></li>         
+           </ul>
+          <hr> 
+
+           <!-- instragram --> 
+            <ul class="social-media-list">
+          
+                   <li><a href="https://www.instagram.com/etecdeguaianazes/" target="_blank" class="contact-icon">
+                     <i class="fa fa-instagram" aria-hidden="true"></i>@etecdeguaianazes</a>
+                   </li>       
+           </ul>
+          <hr>
+      </div>
+  </div>
+  <br><br>
+           <!-- igrame --> 
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.4314718209775!2d-46.401794884475805!3d-23.552942267167754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce65086cafaf55%3A0xf7da96815e7611da!2sETEC%20de%20Guaianases!5e0!3m2!1spt-BR!2sbr!4v1632177457679!5m2!1spt-BR!2sbr"
+              style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+              <br><br>
+
+</section>  
+  
+
+</div>  
+         <!-- footer --> 
+            <footer> <h1>© Todos os direitos reservados.</h1></footer>
+              
+    </body>
+</html>
